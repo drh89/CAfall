60c213db584ffc600b7e4d5d4479adc20c3211a3
Dennis <drh89@live.com>
2019-09-15 13:53:15 +0200

    Members frontend

---
+++ src/main/webapp/scriptMembers.js
@@ -0,0 +1,40 @@
+
+var table = document.getElementById("studentTable");
+var btnReload = document.getElementById("btnReload");
+
+
+
+let dom = "http://localhost:8080/CAfall/api/groupmembers";
+
+
+
+onload = getAllMembers();
+btnReload.addEventListener("click",getAllMembers);
+
+
+function getAllMembers() {
+//    event.preventDefault();
+
+    var conf = {method: "get"};
+    var url = dom + "/all";
+
+    var promise = fetch(url, conf);
+
+    promise.then(res => res.json())
+            .then(function (data) {
+                var html = generateTable(data);
+                table.innerHTML = html;
+
+            });
+}
+
+
+function generateTable(data) {
+    var newData = data.map(function (m) {
+        return "<tr><td>" + m.name + "</td>" + "<td>" + m.studentId + "</td>" +
+                "<td>" + m.color + "</td></tr>";
+    });
+    return "<tr><th>Name</th><th>Student ID</th><th>Color</th><tr>" + newData.join("");
+
+
+}
